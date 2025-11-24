86071dda710b5296907b1481cdf4aa7aa81071dc
jerry <chenyuan.wang@mail.utoronto.ca>
2025-11-24 11:17:05 -0600

    update port

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ app.delete("/api/todos/:id", (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port", PORT));
